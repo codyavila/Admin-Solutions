b4a85a0df5595d3c37aa6981d888564122041a6e
sanderscody26 <sanderscody26@gmail.com>
2022-08-08 16:43:31 -0400

    Added User in show page, not dynamic yet

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './single.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <Sidebar />
+      <div className='singleContainer'>
+        <Navbar />
+        <div className='top'>
+          <div className='left'>
+            <div className="editButton">Edit</div>
+            <h1 className='title'>Information</h1>
+            <div className='item'>
+              <img src='' alt='' className='itemImg' />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+1 (555) 555-5555</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">9903 Someone's Address Ln.</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='right'></div>
+        </div>
+        <div className='bottom'></div>
+      </div>
+    </div>
   )
 }
 
