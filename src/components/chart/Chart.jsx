c47a23537c7bf5901fa129e1ddb67ab04f6543d3
sanderscody26 <sanderscody26@gmail.com>
2022-08-08 15:38:25 -0400

    Added MUI table and temp data

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -23,7 +23,9 @@ const data = [
 const Chart = ({ aspect, title }) => {
   return (
     <div className='chart'>
-      <div className='title'>{title}</div>
+      <div className='title'>
+        {title}
+      </div>
       <ResponsiveContainer width='100%' aspect={aspect}>
         <AreaChart
           width={730}
