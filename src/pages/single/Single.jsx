96ca27ab0996ca4d97be3132c7dbf397af83d7a6
sanderscody26 <sanderscody26@gmail.com>
2022-08-08 17:34:22 -0400

    Layout on show page complete, data not dynamic

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -39,6 +39,7 @@ const Single = () => {
           </div>
         </div>
         <div className='bottom'>
+          <h1 className="title">Latest Transactions</h1>
           <List />
         </div>
       </div>
