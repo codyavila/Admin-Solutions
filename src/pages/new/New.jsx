ea75c4737e54aa7da30db7bc1e056a81ab033456
sanderscody26 <sanderscody26@gmail.com>
2022-08-09 14:39:20 -0400

    Set up form for New page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './new.scss'
 
 const New = () => {
   return (
-    <div>New</div>
+    <div className='new'>
+      <Sidebar />
+      <div className='newContainer'>
+        <Navbar />
+        <div className='top'>
+          <h1>Add User</h1>
+        </div>
+        <div className='bottom'>
+          <div className='left'>
+            <img
+              src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              alt=''
+            />
+          </div>
+          <div className='right'>
+            <form>
+              <div className='formInput'>
+                <label>Username</label>
+                <input type='text' placeholder='Enter Username' />
+              </div>
+              <div className='formInput'>
+                <label>First & Last name</label>
+                <input type='text' placeholder='Enter Name' />
+              </div>
+              <div className='formInput'>
+                <label>Email</label>
+                <input type='text' placeholder='Enter Email' />
+              </div>
+              <div className='formInput'>
+                <label>Phone</label>
+                <input type='text' placeholder='(555) 555-5555' />
+              </div>
+              <div className='formInput'>
+                <label>Password</label>
+                <input type='text' placeholder='Enter Password' />
+              </div>
+              <div className='formInput'>
+                <label>Address</label>
+                <input type='text' placeholder='Enter Address' />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
